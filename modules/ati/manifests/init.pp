5d942c54a3e8e326db66dfc660a09717a4f16291
dima-nixx <dima.nixx@gmail.com>
2013-08-12 14:16:35 +0400

    Update init.pp

--- modules/ati/manifests/init.pp
+++ modules/ati/manifests/init.pp
@@ -1,9 +1,3 @@
-class dropbox {
-  include stdlib
-  include dropbox::config
-
-  anchor { 'dropbox::begin': }
-  -> class { 'dropbox::package': }
-  ~> class { 'dropbox::service': }
-  -> anchor { 'dropbox::end': }
+class ati {
+  
 }
