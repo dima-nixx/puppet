ba41ee77a0fe4a41310bba05199c360d431760ef
dima-nixx <dima.nixx@gmail.com>
2013-08-12 16:24:14 +0400

    добавлена автозагрузка

--- modules/xbmc/manifests/init.pp
+++ modules/xbmc/manifests/init.pp
@@ -5,15 +5,29 @@ class xbmc {
     package {'xbmc': ensure => present}
     package {'xbmc-bin': ensure => present,}
     
+   user { 'xbmc':
+  ensure           => 'present',
+  groups           => ['video', 'audio'],
+  home             => '/home/xbmc/',
+  shell            => '/bin/bash',
+  }
     
-#    file {
-#       '/etc/init//.conf':
-#          ensure => file,
-#         mode   => '0644',
-#        owner  => root,
-#       group  => root,
-#      source => 'puppet:///modules/xbmc/*.conf';
-
+    
+    file { '/etc/lightdm/lightdm.conf':
+        ensure => file,
+        mode   => '0644',
+        owner  => root,
+        group  => root,
+        source => 'puppet:///modules/xbmc/files/lightdm.conf';
+    }
+    
+    file { '/usr/share/xsessions/XBMC.desktop':
+        ensure => file,
+        mode   => '0644',
+        owner  => root,
+        group  => root,
+        source => 'puppet:///modules/xbmc/files/XBMC.desktop';
+    }
 
 }
 
