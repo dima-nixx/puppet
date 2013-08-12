fa421ee51aef7a5625ccce9ffbe00d64b118b366
dima-nixx <dima.nixx@gmail.com>
2013-08-12 13:06:39 +0400

    Чистим мусор

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,16 +1,15 @@
 # vi /etc/puppet/manifests/site.pp
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
-<<<<<<< HEAD
+
 import 'config.pp'
 import 'common.pp'
 import 'folders.pp'
 import 'webmin.pp'
-=======
 import 'common.pp'
 import 'folders.pp'
 import 'webmin.pp'
 import 'dropbox.pp'
->>>>>>> 727ef9ac7b765dfa75bf4817772ac979e20c39db
+
 
 class common {
 include ssh
@@ -30,8 +29,6 @@ include folders
 include webmin
 <<<<<<< HEAD
 #include deluge
-=======
->>>>>>> 727ef9ac7b765dfa75bf4817772ac979e20c39db
 include dropbox
 }
 
