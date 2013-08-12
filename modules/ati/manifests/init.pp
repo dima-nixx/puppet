bfd31dea3531ece02ad6c38473b42103f65942b6
dima-nixx <dima.nixx@gmail.com>
2013-08-12 14:34:29 +0400

    добавлена проверка установки оконного менеджера

--- modules/ati/manifests/init.pp
+++ modules/ati/manifests/init.pp
@@ -1,6 +1,16 @@
 class ati {
       package { "fglrx":
-                ensure => installed,
-        }
-
+            ensure => installed,
+            require => package["lightdm"]'
+      }
+      
+      package {"lightdm":
+            ensure => installed,
+      }
+      
+      package {"lightdm-gtk-greeter":
+            ensure => installed,
+            require => package["lightdm"]'
+      }
+      
 }
