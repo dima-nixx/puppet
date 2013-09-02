aaa48621aad92d9fa3bdbe00d68187a54a4b9739
dima-nixx <dima.nixx@gmail.com>
2013-09-02 17:04:58 +0400

    Create init.pp

---
+++ modules/deluge/manifests/init.pp
@@ -0,0 +1,38 @@
+class xbmc {
+
+  service {'deluged': 
+				ensure => running,
+  			ensure => enable,
+  			require => Package[''],
+				}
+  
+  package {'deluged': ensure => installed,}
+    
+#   user { 'xbmc':
+#        ensure           => 'present',
+#        groups           => ['video', 'audio'],
+#        home             => '/home/xbmc/',
+#        shell            => '/bin/bash',
+#        }
+#    
+#    
+#    
+#    file { 'lightdm.conf':
+#        path => '/etc/lightdm/lightdm.conf',
+#        ensure => file,
+#        mode   => '0644',
+#        owner  => root,
+#        group  => root,
+#        source => 'puppet:///modules/xbmc/files/lightdm.conf';
+#    }
+#    
+#    file { 'XBMC.desktop':
+#        path => '/usr/share/xsessions/XBMC.desktop',
+#        ensure => file,
+#        mode   => '0644',
+#        owner  => root,
+#        group  => root,
+#        source => 'puppet:///modules/xbmc/files/XBMC.desktop';
+#    }
+
+}
